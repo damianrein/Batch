67e4bf726a69c5a89cb28e7df1b80fc45b3aeb14
damianrein <damianrein464@gmail.com>
2024-07-24 23:40:17 -0300

    -

--- src/main/java/com/Batch/batch/ValidateAccountTasklet.java
+++ src/main/java/com/Batch/batch/ValidateAccountTasklet.java
@@ -1,5 +1,17 @@
 package com.Batch.batch;
 
-public class ValidateAccountTasklet {
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class ValidateAccountTasklet implements Tasklet {
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		
+		boolean filterIsAproved = true;
+		return null;
+	}
 
 }

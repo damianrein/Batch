2f174adf1c8137870dbbdc4a907955c52a833102
damianrein <damianrein464@gmail.com>
2024-07-26 19:19:04 -0300

    -

--- src/main/java/com/Batch/batch/CancelTransactionTasklet.java
+++ src/main/java/com/Batch/batch/CancelTransactionTasklet.java
@@ -1,18 +1,35 @@
 package com.Batch.batch;
 
+import org.jboss.logging.Logger;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.Batch.repositories.ITransferPaymentRepository;
+
 
 public class CancelTransactionTasklet implements Tasklet {
 
-	@Override
-	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		
-		boolean filterIsAproved = true;
-		return null;
-	}
+	//Logger log = Logger.getLogger(CancelTransactionTasklet.class);
+    @Autowired
+    private ITransferPaymentRepository transferPaymentRespository;
 
+    @Override
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
 
+        String transactionId = chunkContext.getStepContext()
+                .getStepExecution()
+                .getJobParameters()
+                .getString("transactionId");
+
+        String errorMessage = chunkContext.getStepContext()
+                                          .getStepExecution()
+                                          .getJobExecution()
+                                          .getExecutionContext()
+                                          .getString("message");
+
+        return RepeatStatus.FINISHED;
+    }
 }

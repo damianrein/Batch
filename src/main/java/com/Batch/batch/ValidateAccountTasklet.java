939de94010080304d32a963ba0608cd643ef51ee
damianrein <damianrein464@gmail.com>
2024-08-07 22:16:51 -0300

    -

--- src/main/java/com/Batch/batch/ValidateAccountTasklet.java
+++ src/main/java/com/Batch/batch/ValidateAccountTasklet.java
@@ -4,13 +4,20 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.Batch.repositories.ITransferPaymentRepository;
 
 public class ValidateAccountTasklet implements Tasklet {
 
+	@Autowired
+	private ITransferPaymentRepository transferPaymentRepository;
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 		
 		boolean filterIsAproved = true;
+		
+		
 		return null;
 	}
 
